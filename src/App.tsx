4d005269acbb5c78b06cca8566ecdefef582526a
Nathanael DJ <nathanael.c2pank@gmail.com>
2026-01-23 15:43:27 +0700

    include fontsource/roboto, react-router, axios, fe setup chapter done

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,38 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import CssBaseLine from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Route, Routes } from "react-router"; //react-router-dom is not needed as of v6
+import Applayout from "./components/layout/Applayout";
+import Authlayout from "./components/layout/Authlayout";
+import Homepage from "./pages/Homepage";
+import Boardpage from "./pages/Boardpage";
+import Loginpage from "./pages/Loginpage";
+import Signuppage from "./pages/Signuppage";
 
 function App() {
-  return <div></div>;
+  const theme = createTheme({
+    palette: { mode: "light" },
+  });
+  <ThemeProvider theme={theme}>
+    <CssBaseLine />
+    <BrowserRouter>
+      <Routes>
+        // Authlayout routes
+        <Route path="/" element={<Authlayout />}>
+          <Route path="login" element={<Loginpage />} />
+          <Route path="signup" element={<Signuppage />} />
+        </Route>
+        // Applayout routes
+        <Route path="/" element={<Applayout />}>
+          <Route index element={<Homepage />} />
+          <Route path="boards" element={<Homepage />} />
+          <Route path="boards/:boardId" element={<Boardpage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>;
 }
 
 export default App;
---
+++ src/components/layout/Applayout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router";
+
+const Applayout = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Applayout;
